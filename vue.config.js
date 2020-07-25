e4fcae07157ce53ef15237ccf6337ee165234b53
mohwan <chaoui.mar2@gmail.com>
2020-07-25 11:27:58 +0100

    logo pwa

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   lintOnSave: false,
   pwa: {
+    iconPaths: {
+      favicon32: 'img/mw.png',
+      favicon16: 'img/mw.png',
+      appleTouchIcon: 'img/mw.png',
+      maskIcon: 'img/mw.png',
+      msTileImage: 'img/mw.png'
+    },
     themeColor: '#3273dc'
   }
 }
